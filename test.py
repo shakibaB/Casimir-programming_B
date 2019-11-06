0c4fdd7b9b3662d628b689a52cdf3ddb27d6e054
shakibaB <bahaareh.shakiba@gmail.com>
2019-11-06 13:31:04 +0100

    my third commit

--- test.py
+++ test.py
@@ -3,8 +3,17 @@ print('Yay :P')
 
 import numpy as np
 
+
+def circum(r):
+    '''The circle circumfrance is:'''
+    return 2 * np.pi * r 
+
 def area(r):
     '''The area of the circle is:'''
     return 2 * np.pi * r
 
+
+
+
 print(area.__doc__)
+print(cirdum.__doc__)
